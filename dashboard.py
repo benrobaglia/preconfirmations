a3a4e0181f89dd44fe4131daa6f9391ad1998072
Benoit ROBAGLIA <benoitrobaglia@gmail.com>
2024-12-16 14:59:44 +0000

    initial commit

---
+++ dashboard.py
@@ -0,0 +1,168 @@
+import streamlit as st
+import pandas as pd
+import seaborn as sns
+import matplotlib.pyplot as plt
+from utils import *
+
+# Sidebar Navigation
+st.sidebar.title("Navigation")
+pages = ["Presentation", "Eligibility Transactions", "Model Configuration"]
+page = st.sidebar.radio("", pages)
+
+# 1️⃣ Presentation Page
+if page == "Presentation":
+    st.title("Chorus One's Preconfirmation Pricing Model")
+    st.markdown("""
+    Lorem ipsum dolor sit amet, consectetur adipiscing elit. Maecenas pulvinar sapien quam, at vestibulum lacus egestas ut. Maecenas in blandit nisl. Nullam tincidunt, nisi vel blandit varius, nisi tortor hendrerit felis, ac condimentum mauris orci ut magna. In et mattis est. Suspendisse convallis in magna non bibendum. Praesent id nisi blandit, hendrerit lectus nec, ornare odio. Sed velit sapien, eleifend eget nibh eu, aliquam dignissim diam. Morbi viverra diam id est posuere sagittis. Mauris euismod massa sed magna euismod, quis scelerisque dolor vehicula. Aliquam semper lectus sem, quis ullamcorper nisl finibus nec. Quisque vel semper orci, ut viverra tortor. Phasellus varius enim non pharetra viverra. In vehicula vehicula nunc in facilisis. Sed tortor libero, egestas dapibus ullamcorper ut, porttitor et nisi. In eget tellus pharetra odio luctus gravida.
+    """)
+
+elif page == "Eligibility Transactions":
+    st.header('Preconfirmable Transactions')
+    
+    with st.form(key="eligibility_form"):
+        start_position = st.slider('Start Position', min_value=1, max_value=200, value=10, step=1, help="Set the starting position for eligible transactions.")
+        end_position = st.slider('End Position', min_value=1, max_value=200, value=150, step=1, help="Set the ending position for eligible transactions.")
+        submitted = st.form_submit_button("Run")
+    
+    if submitted:
+        tx_data = pd.read_csv('/home/research_group/preconfirmations/txs_sample.csv')
+        eligible_txs = tx_data.loc[(tx_data['position'] > start_position) & (tx_data['position'] < end_position)]
+        
+        st.header("Key Metrics")
+        percentage_of_eligible_txs = len(eligible_txs) / len(tx_data) if len(tx_data) > 0 else 0
+        st.metric(label="Percentage of Eligible Transactions for Preconfirmations", value=f"{percentage_of_eligible_txs:.2%}", help="This shows the percentage of transactions that are eligible for preconfirmations based on the position range.")
+        
+        st.header("Priority Fee Distribution for Eligible Transactions")
+        fig, ax = plt.subplots()
+        sns.histplot(eligible_txs['priority_fee_per_gas'].loc[eligible_txs['priority_fee_per_gas'] < 10e-9] * 1e9, bins=20, ax=ax)
+        ax.set_xlabel("Priority Fee per Gas (in Gwei)")
+        ax.set_title("Distribution of Priority Fee per Gas")
+        st.pyplot(fig)
+        
+        csv = eligible_txs.to_csv(index=False)
+        st.download_button(
+            label="Download Eligible Transactions as CSV",
+            data=csv,
+            file_name='eligible_transactions.csv',
+            mime='text/csv',
+        )
+
+
+# 3️⃣ Model Configuration Page
+elif page == "Model Configuration":
+    st.header("3. Models Configuration")
+    
+    lags = np.arange(1, 33, 1)
+    eligible_txs = pd.read_csv('/home/research_group/preconfirmations/eligible_txs_sample.csv')
+    block_features = pd.read_csv('/home/research_group/preconfirmations/block_features.csv')
+    agg_df = build_agg_features(eligible_txs, lags)
+    final_df = eligible_txs.merge(agg_df, on=['block_number', 'tx_topology']).merge(block_features, on='block_number')
+
+    # Initialize session state for model configuration
+    if 'quantile' not in st.session_state:
+        st.session_state['quantile'] = 50
+    if 'max_window' not in st.session_state:
+        st.session_state['max_window'] = 10
+    if 'lr_features' not in st.session_state:
+        st.session_state['lr_features'] = ['q50_priority_fee_per_gas']
+    if 'max_window_lr' not in st.session_state:
+        st.session_state['max_window_lr'] = (1, 10)
+    if 'lr_training_threshold' not in st.session_state:
+        st.session_state['lr_training_threshold'] = 21331500
+
+    tabs = st.tabs(["Quantile Heuristic", "Linear Regression", "Neural Network"])
+    
+    with tabs[0]:  # Quantile Heuristic Configuration
+        st.subheader("Configure Quantile Heuristic Model")
+        
+        # Define the widgets and their callbacks
+        quantile = st.selectbox(
+            "Select Quantile", 
+            [i for i in range(10, 100, 10)], 
+            index=[i for i in range(10, 100, 10)].index(st.session_state['quantile']),
+            key="quantile",
+            on_change=lambda: st.session_state.update({'quantile': st.session_state['quantile']})
+        )
+
+        max_window = st.slider(
+            "Select Maximum Window Size", 
+            min_value=1, 
+            max_value=32, 
+            value=st.session_state['max_window'], 
+            key="max_window", 
+            on_change=lambda: st.session_state.update({'max_window': st.session_state['max_window']})
+        )
+
+        if st.button("Save Configuration for Quantile Heuristic"):
+            st.success(f"Configuration for Quantile Heuristic model saved! (Quantile: {st.session_state['quantile']}, Max Window: {st.session_state['max_window']})")
+            
+    with tabs[1]:  # Linear Regression Configuration
+        st.subheader("Configure Linear Regression Model")
+
+        # Define the widgets and their callbacks
+        feats = [f'q{q}_priority_fee_per_gas' for q in range(10, 100, 10)]
+        feats2 = ['mean_priority_fee_per_gas', 'min_priority_fee_per_gas', 'max_priority_fee_per_gas', 'skew_priority_fee_per_gas']
+        indiv = ['gas_used']
+        blocks = ['block_gas_used_log_1']
+        
+        features = st.multiselect(
+            "Select Features", 
+            feats+feats2+indiv+blocks, 
+            default=st.session_state['lr_features'], 
+            key='lr_features', 
+            on_change=lambda: st.session_state.update({'lr_features': st.session_state['lr_features']})
+        )
+
+        lags = st.slider(
+            "Select Max Window Size for the Features", 
+            min_value=1, 
+            max_value=32,
+            value=st.session_state['max_window_lr'],
+            key='max_window_lr', 
+            on_change=lambda: st.session_state.update({'max_window_lr': st.session_state['max_window_lr']})
+        )
+
+
+        training_threshold = st.slider(
+            "Select Training Threshold", 
+            min_value=21330584, 
+            max_value=21332578, 
+            value=st.session_state['lr_training_threshold'], 
+            key='lr_training_threshold', 
+            on_change=lambda: st.session_state.update({'lr_training_threshold': st.session_state['lr_training_threshold']})
+        )
+
+        if st.button("Save Configuration for Linear Regression"):
+            st.success("Configuration for Linear Regression model saved!")
+
+    with tabs[2]:  # Quantile Heuristic Configuration
+        st.subheader("Configure Neural Network Model")
+
+    print(f"Quantiles: {st.session_state['quantile']}")
+    print(f"Max Window: {st.session_state['max_window']}")
+    print(f"lr_features: {st.session_state['lr_features']}")
+    print(f"max_window_lr: {st.session_state['max_window_lr']}")
+    print(f"lr_training_threshold: {st.session_state['lr_training_threshold']}")
+        
+    quantile_lags = np.arange(1, st.session_state['max_window'])
+    quantile_estimator = rolling_mean_block_pf_estimator(final_df, f"q{st.session_state['quantile']}", quantile_lags)
+    global_quantile_metrics, groups_quantile_metrics = compute_metrics(quantile_estimator)
+
+    global_lr_metrics = {
+        'mae': np.random.uniform(0.5, 2.5),
+        'avg_preconfirmed_errors': np.random.uniform(0.5, 2.5),
+        'preconfirmations_eligible': np.random.rand(),
+        'preconf_value': np.random.uniform(10, 50)
+    }
+    global_nn_metrics = {
+        'mae': np.random.uniform(0.5, 2.5),
+        'avg_preconfirmed_errors': np.random.uniform(0.5, 2.5),
+        'preconfirmations_eligible': np.random.rand(),
+        'preconf_value': np.random.uniform(10, 50)
+    }
+    global_results = [global_quantile_metrics, global_lr_metrics, global_nn_metrics]
+    
+    global_results = pd.DataFrame(global_results, index=['Quantile Heuristic', 'Linear Regression', 'Neural Network'])
+    
+    st.dataframe(global_results)
+    
---
+++ utils.py
@@ -0,0 +1,217 @@
+import numpy as np
+import pandas as pd
+from sklearn.linear_model import LinearRegression
+from sklearn.metrics import mean_squared_error, mean_absolute_error
+
+
+def compute_metrics(y_results):
+    """
+    Columns in y_results (already filtered by eligible txs):
+    y_true | y_pred | block_number | eligible | tx_topology | gas_used
+    """
+    # Preconf txs
+    preconf_txs = y_results.loc[y_results['y_true'] > y_results['y_pred']]
+    
+    # MAE (Mean Absolute Error)
+    mae = np.mean(np.abs(y_results['y_true'] - y_results['y_pred']))
+    
+    # MSE (Mean Squared Error)
+    mse = np.mean((y_results['y_true'] - y_results['y_pred']) ** 2)
+
+    # Avg of differences (y_true - y_pred)
+    avg_preconfirmed_errors = (y_results['y_true'] - y_results['y_pred']).clip(lower=0).mean()
+
+    # Avg nb of preconfirmations (when y_true > y_pred)
+    preconfirmations_eligible = (y_results['y_true'] > y_results['y_pred']).mean()
+
+    # Total nb of preconf
+    preconfirmations = len(preconf_txs)
+    
+    # Preconfirmable value
+    preconf_value = (preconf_txs['y_true'] * preconf_txs['gas_used']).sum()
+    
+    # Compute the metrics for each tx_topology group
+    grouped_metrics = y_results.groupby('tx_topology').apply(lambda group: {
+        'avg_preconfirmed_errors': (group['y_true'] - group['y_pred']).clip(lower=0).mean() * 1e9,
+        'preconfirmations_eligible': (group['y_true'] > group['y_pred']).mean(),
+        'preconf_value': group.loc[group.y_true > group.y_pred][['y_true', 'gas_used']].prod(axis=1).sum()
+    }).to_dict()
+    
+    metrics = {
+        'mae': mae * 1e9,
+        'avg_preconfirmed_errors': avg_preconfirmed_errors * 1e9,
+        'preconfirmations_eligible': preconfirmations_eligible,
+        'preconf_value': preconf_value
+    }
+
+    return metrics, grouped_metrics
+
+
+def compute_lags_stat(df, feature='priority_fee_per_gas', statistic = 'mean', lags = [1, 2, 3, 4, 5]):
+    # Validate statistic argument
+    valid_statistics = ['mean', 'median', 'q25', 'q75', 'q60']
+    
+    if statistic not in valid_statistics:
+        raise ValueError(f"Invalid statistic '{statistic}'. Choose from {valid_statistics}.")
+    
+    # Group by block_number and tx_topology to calculate the chosen statistic for priority_fee_per_gas
+    if statistic == 'mean':
+        agg = (
+            df.groupby(['tx_topology', 'block_number'])[feature]
+            .mean().to_frame(f'{statistic}_{feature}').sort_index(level=0)
+        )
+    elif statistic == 'median':
+        agg = (
+            df.groupby(['tx_topology', 'block_number'])[feature]
+            .median()
+            .to_frame(f'{statistic}_{feature}').sort_index(level=0)
+        )
+    elif statistic == 'q25':
+        agg = (
+            df.groupby(['tx_topology', 'block_number'])[feature]
+            .quantile(0.25)
+            .to_frame(f'{statistic}_{feature}').sort_index(level=0)
+        )
+    elif statistic == 'q75':
+        agg = (
+            df.groupby(['tx_topology', 'block_number'])[feature]
+            .quantile(0.75)
+            .to_frame(f'{statistic}_{feature}').sort_index(level=0)
+        )
+    elif statistic == 'q60':
+        agg = (
+            df.groupby(['tx_topology', 'block_number'])[feature]
+            .quantile(0.60)
+            .to_frame(f'{statistic}_{feature}').sort_index(level=0)
+        )
+    
+    for lag in lags:
+        agg[f'{statistic}_{feature}_lag_{lag}'] = agg.groupby(level='tx_topology')[f'{statistic}_{feature}'].shift(lag)
+    
+    return agg
+
+
+def last_block_pf_estimator(df, stat):
+    df = df.rename(columns={f'{stat}_priority_fee_per_gas_lag_1': 'y_pred', 'priority_fee_per_gas': 'y_true'})
+    return df[['block_number', 'y_true', 'y_pred', 'tx_topology', 'gas_used']].dropna().reset_index(drop=True)
+
+
+def rolling_mean_block_pf_estimator(df, stat, lags):
+    df[f'{stat}_pfpg_rolling_{len(lags)}'] = df[[f'{stat}_priority_fee_per_gas_lag_{l}' for l in lags]].mean(1)
+    df = df.rename(columns={f'{stat}_pfpg_rolling_{len(lags)}': 'y_pred', 'priority_fee_per_gas': 'y_true'})
+    return df[['block_number', 'y_true', 'y_pred', 'tx_topology', 'gas_used']].dropna().reset_index(drop=True)
+
+
+def build_block_features(df):
+    block_metrics = df.groupby('block_number').agg(
+        block_gas_used=('gas_used', 'sum'),
+        tx_count=('gas_used', 'size')
+    ).reset_index()
+    block_metrics['block_gas_used_log_1'] = block_metrics['block_gas_used'].shift(1)
+    block_metrics['tx_count_log_1'] = block_metrics['tx_count'].shift(1)
+    return block_metrics
+
+
+def build_agg_features(df, lags=[1, 2, 3, 4, 5]):
+    """
+    Builds aggregated features for the priority fee per gas for each block and each tx topology.
+    
+    Args:
+        df (DataFrame): A DataFrame containing transaction data with at least 'block_number', 'tx_topology', and 'priority_fee_per_gas' columns.
+        lags (list): A list of lags to apply to the aggregated features.
+    
+    Returns:
+        DataFrame: A DataFrame with the original data and the new aggregated features and their respective lags.
+    """
+    
+    # 1️⃣ Compute Quantile Features (all at once)
+    quantiles = np.arange(0.1, 1, 0.1)
+    quantile_agg = (
+        df.groupby(['tx_topology', 'block_number'])['priority_fee_per_gas']
+        .quantile(quantiles)
+        .unstack(level=-1)
+    )
+    # Rename quantile columns to reflect quantile names
+    quantile_agg.columns = [f'q{int(quantile * 100)}_priority_fee_per_gas' for quantile in quantiles]
+    
+    # 2️⃣ Compute Additional Aggregate Features (mean, min, max, skew)
+    additional_agg = df.groupby(['tx_topology', 'block_number'])['priority_fee_per_gas'].agg(
+        mean_priority_fee_per_gas='mean',
+        min_priority_fee_per_gas='min',
+        max_priority_fee_per_gas='max',
+        skew_priority_fee_per_gas='skew'
+    )
+    
+    # 3️⃣ Combine all aggregate features
+    agg = pd.concat([quantile_agg, additional_agg], axis=1)
+    
+    # 4️⃣ Generate Lags for All Columns
+    lagged_features = []
+    for lag in lags:
+        shifted_agg = agg.groupby(level='tx_topology').shift(lag)
+        shifted_agg.columns = [f'{col}_lag_{lag}' for col in shifted_agg.columns]
+        lagged_features.append(shifted_agg)
+    
+    # 5️⃣ Concatenate the original features with their lags
+    final_agg = pd.concat([agg] + lagged_features, axis=1)
+    
+    # Reset index to flatten the index into columns
+    return final_agg.reset_index()
+
+
+
+def train_linear_regression(df, features, features_lag, lags, training_threshold, target='priority_fee_per_gas'):
+    """
+    Trains a Linear Regression model using lagged features.
+    
+    Args:
+        df (DataFrame): The DataFrame containing all data with features and lags.
+        features (list): List of base features to be used as predictors.
+        lags (list): List of lags to be applied to each feature.
+        training_threshold (int): The block number threshold to split the train and test data.
+        target (str): The name of the target variable column (default is 'priority_fee_per_gas').
+        
+    Returns:
+        dict: A dictionary containing the trained model, predictions, and metrics.
+    """
+    # 1️⃣ Generate the feature columns from features + lags
+    feature_columns = [f'{feature}_lag_{lag}' for feature in features_lag for lag in lags]
+    feature_columns += features
+    
+    # Ensure that the required feature columns exist in the DataFrame
+    missing_features = [col for col in feature_columns if col not in df.columns]
+    if missing_features:
+        raise ValueError(f"The following required features are missing from the DataFrame: {missing_features}")
+    
+    # 2️⃣ Split the data into training and testing sets
+    train_data = df[df['block_number'] <= training_threshold]
+    test_data = df[df['block_number'] > training_threshold]
+    
+    # Remove any rows with NaN values (caused by shift() operations)
+    train_data = train_data.dropna(subset=feature_columns + [target])
+    test_data = test_data.dropna(subset=feature_columns + [target])
+    
+    # 3️⃣ Extract X (features) and y (target) for training and testing
+    X_train = train_data[feature_columns]
+    y_train = train_data[target]
+    X_test = test_data[feature_columns]
+    y_test = test_data[target]
+    
+    # 4️⃣ Train the Linear Regression model
+    model = LinearRegression()
+    model.fit(X_train, y_train)
+    
+    # 5️⃣ Make predictions on the test data
+    y_pred = model.predict(X_test)
+    
+    r2 = model.score(X_test, y_test)
+    
+    # 7️⃣ Return the model, predictions, and metrics
+    results = pd.DataFrame()
+    results['y_pred'] = y_pred
+    results['y_true'] = y_test.reset_index(drop=True)
+    results['block_number'] = test_data['block_number'].reset_index(drop=True)
+    results['tx_topology'] = test_data['tx_topology'].reset_index(drop=True)
+    results['gas_used'] = test_data['gas_used'].reset_index(drop=True)
+    return results, r2
+
